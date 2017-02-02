a4e48d263c8dcc0af0e8180eec1148f33bb78b9c
jakecronin <jake.cronin@emory.edu>
2017-02-02 18:31:58 -0500

    first commit

--- Project Loca/ViewController.swift
+++ Project Loca/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
 	override func viewDidLoad() {
+		print("hello world")
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
